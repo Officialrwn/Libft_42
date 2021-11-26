dae87137fdcfe08cfb68b1d61a89e5571ccac871
Leo Tran <leotran@c2r4p1.hive.fi>
2021-11-26 15:43:21 +0200

    fixed free

--- src/ft_lstdel.c
+++ src/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: leotran <leotran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 15:17:45 by leotran           #+#    #+#             */
-/*   Updated: 2021/11/22 09:54:25 by leotran          ###   ########.fr       */
+/*   Updated: 2021/11/26 15:43:01 by leotran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,9 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	{
 		head = temp->next;
 		(*del)(temp->content, temp->content_size);
+		free(temp->content);
+		temp->content = NULL;
 		free(temp);
-		temp = NULL;
 		temp = head;
 	}
 	*alst = NULL;
