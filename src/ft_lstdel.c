4025eb492e118219865460c3388d598d10bbd11f
Leo Tran <leotran@c2r4p1.hive.fi>
2021-11-26 16:36:27 +0200

    fix lstdel

--- src/ft_lstdel.c
+++ src/ft_lstdel.c
@@ -6,7 +6,7 @@
 /*   By: leotran <leotran@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 15:17:45 by leotran           #+#    #+#             */
-/*   Updated: 2021/11/26 15:43:01 by leotran          ###   ########.fr       */
+/*   Updated: 2021/11/26 15:59:38 by leotran          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 	{
 		head = temp->next;
 		(*del)(temp->content, temp->content_size);
-		free(temp->content);
-		temp->content = NULL;
 		free(temp);
 		temp = head;
 	}
