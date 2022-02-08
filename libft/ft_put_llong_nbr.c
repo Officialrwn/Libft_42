cc5e0b21534037b91ce98b41c20eb3bbb02296aa
Leo MPB <leotran@student.hive.fi>
2022-02-08 03:20:21 +0200

    Added put_llong_nbr.c put_addr.c itoa_base.c [skip ci]

--- libft/ft_put_llong_nbr.c
+++ libft/ft_put_llong_nbr.c
@@ -1,25 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_put_llong_nbr.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/06 14:47:16 by leo               #+#    #+#             */
-/*   Updated: 2022/02/08 03:18:37 by leo              ###   ########.fr       */
+/*   Created: 2021/11/05 14:54:57 by leotran           #+#    #+#             */
+/*   Updated: 2022/02/08 03:17:52 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft/libft.h"
-#include <stdio.h>
+#include "libft.h"
 
-int main (void)
+void	ft_put_llong_nbr(long long llnum)
 {
-	int a = 5;
-	printf("printf real: %p\n", &a);
-	printf("%lld\n", (long long) &a);
-	ft_itoa_base((long long) &a, 16);
-	ft_put_llong_nbr((long long) &a);
-	ft_putaddr((long long) &a);
-	return (0);
+	long long	i;
+	int			count;
+
+	i = 1;
+	count = 0;
+	if (llnum < 0)
+	{
+		ft_putchar('-');
+		llnum *= -1;
+	}
+	while (i <= (llnum / 10))
+	{
+		i = i * 10;
+		count++;
+	}
+	while (count > 0)
+	{
+		ft_putchar((llnum / i) + '0');
+		llnum %= i;
+		i /= 10;
+		count--;
+	}
+	ft_putchar(llnum + '0');
 }
--- libft/ft_putaddr.c
+++ libft/ft_putaddr.c
@@ -1,42 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_put_ullong_nbr.c                                :+:      :+:    :+:   */
+/*   ft_putaddr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/05 14:54:57 by leotran           #+#    #+#             */
-/*   Updated: 2022/02/08 01:26:38 by leo              ###   ########.fr       */
+/*   Created: 2022/02/08 02:22:44 by leo               #+#    #+#             */
+/*   Updated: 2022/02/08 03:09:14 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_put_ullong_nbr(unsigned long long n)
-{
-	unsigned long long	num;
-	unsigned long long	i;
-	int					count;
+/*
+** Cast the parameter to ((long long) &llnum) 
+** when calling the function to retrieve the 
+** memory address as LLONG and convert to 0x.
+*/
 
-	num = n;
-	i = 1;
-	count = 0;
-	if (num < 0)
-	{
-		ft_putchar('-');
-		num *= -1;
-	}
-	while (i <= (num / 10))
-	{
-		i = i * 10;
-		count++;
-	}
-	while (count > 0)
-	{
-		ft_putchar((num / i) + '0');
-		num %= i;
-		i /= 10;
-		count--;
-	}
-	ft_putchar(num + '0');
+void	ft_putaddr(long long llnum)
+{
+	char		*mem_addr;
+
+	mem_addr = ft_itoa_base(llnum, 16);
+	ft_putstr("0x");
+	ft_putstr(mem_addr);
 }
